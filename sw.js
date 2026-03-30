cbb934034789e9207529c27e5ef34446f9d77208
김태경 <user@AL02261929-4.local>
2026-03-30 19:10:39 +0900

    ✨ 퀴즈/대결 진행 중 돌아가기 버튼 추가

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nihongo-v2';
+const CACHE = 'nihongo-v3';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
