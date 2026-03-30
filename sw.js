bd6a2815d46881263825efe159602ff91a2f3ccf
김태경 <user@AL02261929-4.local>
2026-03-30 19:30:09 +0900

    🔧 PWA 설치 오류 수정 (경로 상대경로 변환)

--- sw.js
+++ sw.js
@@ -1,5 +1,15 @@
-const CACHE = 'nihongo-v5';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'nihongo-v6';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
